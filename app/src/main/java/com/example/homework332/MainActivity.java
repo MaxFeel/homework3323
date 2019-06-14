49ca1ed11572c08ad99db3bf249c2b4215c67730
maximtosh <mr.maks.filippov.94@mail.ru>
2019-06-14 20:54:43 +0300

    Initial commit

---
+++ app/src/main/java/com/example/homework332/MainActivity.java
@@ -0,0 +1,124 @@
+package com.example.homework332;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    Spinner languagesSp;
+    Spinner themeSp;
+    Button  btnOk;
+    Locale locale;
+    int    changedTheme;
+    Activity activity;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.buttonColor).setOnClickListener(okListner);
+
+        btnOk = findViewById(R.id.button);
+        initLanguage();
+
+
+    }
+
+    private final View.OnClickListener okListner = new View.OnClickListener() {
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.buttonColor:
+                    Utils.changeToTheme(MainActivity.this, changedTheme);
+            }
+        }
+    };
+
+
+
+    void initLanguage(){
+        languagesSp = (Spinner)findViewById(R.id.languagesSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.languages, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        languagesSp.setAdapter(adapter);
+
+        AdapterView.OnItemSelectedListener itemSelectedListener = new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                String item = (String)parent.getItemAtPosition(position);
+                switch (item){
+                    case "Russian" :
+                        locale = new Locale("ru");
+                        break;
+                    case "English" :
+                        locale = new Locale("en");
+                        break;
+                    case "German":
+                        locale = new Locale("de");
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        };
+        languagesSp.setOnItemSelectedListener(itemSelectedListener);
+
+        themeSp = findViewById(R.id.colorSpinner);
+        ArrayAdapter<CharSequence> adapterColor = ArrayAdapter.createFromResource(this,R.array.themes, android.R.layout.simple_spinner_item);
+        adapterColor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        themeSp.setAdapter(adapterColor);
+
+        AdapterView.OnItemSelectedListener changeColorSelcetedListner = new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String item = (String)parent.getItemAtPosition(position);
+                switch (item){
+                    case "Green": Utils.changeThemeSpinner(Utils.THEME_GREEN);
+                        break;
+                    case "Black": Utils.changeThemeSpinner(Utils.THEME_BLACK);
+                        break;
+                    case "Blue": Utils.changeThemeSpinner(Utils.THEME_BLUE);
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        };
+
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Configuration configuration = new Configuration();
+                configuration.setLocale(locale);
+                getResources().updateConfiguration(configuration, getBaseContext().getResources().getDisplayMetrics());
+                recreate();
+            }
+        });
+
+
+    }
+
+
+}
